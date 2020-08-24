afe55bfa2dd3c494e0cfa9347489d07e40ce450e
davis-busteed <davis.busteed@rxa.io>
2020-08-24 12:45:51 -0600

    progres bar, request sleep

--- R/dev.R
+++ R/dev.R
@@ -7,36 +7,41 @@
 library(httr)
 library(jsonlite)
 library(rvest)
+library(progress)
+
+token <- Sys.getenv("RSIG_TOKEN")
+sid <- 84
 
 get_signal_dev <- function(access_token, signal_id) 
 {
-  auth <- paste0("Bearer ", access_token)
-
-  print(sprintf("requesting page: %d", 1))
-  
   url <- sprintf("https://app.readysignal.com/api/signals/%s/output?page=1", signal_id)
+ 
+  auth <- paste0("Bearer ", access_token)
   sesh <- html_session(url, add_headers(Authorization=auth))
   
   json <- jsonlite::fromJSON(httr::content(sesh$response, "text", encoding="UTF8"))
+  data <- json$data
   
-  print(sprintf("received page: %d", 1))
-  cat("\n")
-
-  # data <- json$data
-  data <- c(1,2,3)
-    
+  # make the progress bar if gonna
+  # be needing to paginate
+  if (json$last_page > 1) {
+    pb <-progress_bar$new(
+      format=" [:bar] :percent / :elapsed",
+      total=json$last_page-1,
+      clear=FALSE,
+      width=60
+    )
+  }
+  
   while (json$current_page < json$last_page) {
-    print(sprintf("requesting page: %d", json$current_page+1))
-    
     url <- sprintf("https://app.readysignal.com/api/signals/%s/output?page=%d", signal_id, json$current_page + 1)
     sesh <- jump_to(sesh, url)
+    
     json <- jsonlite::fromJSON(httr::content(sesh$response, "text", encoding="UTF8"))
+    data <- rbind(data, json$data)
     
-    print(sprintf("received page: %d", json$current_page))
-    cat("\n")
-    
-    # data <- rbind(data, json$data)
-    data <- rbind(data, c(1,2,3))
+    pb$tick()
+    Sys.sleep(1) # TODO, only sleep if HTTP 429
   }
   
   return(data)
